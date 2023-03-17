9cf85fbecee2f0e1a87c9b0c83536e5865398575
tonhão mototaxi <jean.carlo.polo@uel.br>
2023-03-17 03:05:15 -0300

    make pack

--- src/formas/tipos.c
+++ src/formas/tipos.c
@@ -1,11 +1,13 @@
 #include "tipos.h"
 
+/** Todas as structs de formas devem ter o primeiro membro como o enum TipoForma
+para que seja possível identificar o tipo de forma */
 struct formaGenerica
 {
     enum TipoForma tipo;
 };
 
-enum TipoForma getTipoForma(void* forma)
+enum TipoForma getTipoForma(void *forma)
 {
-    return ((struct formaGenerica*)forma)->tipo;
+    return ((struct formaGenerica *)forma)->tipo;
 }
